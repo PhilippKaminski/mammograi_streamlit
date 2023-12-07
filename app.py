3657fc0a828c869d3befc7c60b0d0a76458292cd
Philipp Kaminski <PhilippKaminski@outlook.de>
2023-12-07 10:54:33 +0100

    app.py

--- app.py
+++ app.py
@@ -40,6 +40,30 @@ def display_prediction(prediction):
     st.write(f"Prediction: {tumor_types[predicted_class]}")
     for class_index, tumor_type in tumor_types.items():
         st.write(f"{tumor_type}: {prediction[0][class_index] * 100:.2f}%")
+        
+        
+def display_consultation(prediction):
+    tumor_types = {0: "Normal", 1: "Benign", 2: "Malignant"}
+    predicted_class = np.argmax(prediction)
+    
+    if predicted_class == 0:
+        st.subheader("Result: Normal. No evidence of breast cancer detected.")
+        st.write("It's important to note that AI classifications may have a margin of error, and regular mammograms, as recommended by healthcare professionals, are still advised. Consult with your healthcare provider for personalized guidance on screening intervals, typically recommended annually for women aged 40 and older.")
+    elif predicted_class == 1:
+        st.subheader("Result: Benign. No signs of malignant features observed.")
+        st.write("While the AI assessment suggests a benign condition, it's essential to acknowledge the possibility of false positives. Options to proceed include:")
+        st.write("1. **Consultation with a Specialist:** Schedule an appointment with a breast health specialist for a thorough clinical evaluation.")
+        st.write("2. **Follow-up Imaging:** Additional imaging studies, such as ultrasound or MRI, may be recommended to further assess the nature of the identified benign features.")
+        st.write("3. **Biopsy if Necessary:** If uncertainties persist, a biopsy might be suggested to obtain a definitive diagnosis.")
+        st.write("Always consult with your healthcare provider to determine the most appropriate course of action based on your individual circumstances.")
+    elif predicted_class == 2:
+        st.subheader("Result: Malignant. Indications of potential breast cancer detected.")
+        st.write("It's crucial to understand that AI results may have false negatives, and confirmation by medical professionals is essential. Options to consider following a malignant diagnosis:")
+        st.write("1. **Consultation with Oncologist:** Schedule an immediate consultation with an oncologist to discuss the diagnosis and develop a personalized treatment plan.")
+        st.write("2. **Biopsy Confirmation:** Confirm the malignancy through a biopsy, which provides detailed information about the type and stage of the cancer.")
+        st.write("3. **Treatment Planning:** Collaborate with healthcare professionals to formulate a comprehensive treatment strategy, including surgery, chemotherapy, radiation, or a combination, depending on the specifics of the case.")
+        st.write("Prompt and comprehensive medical intervention is crucial; consult with your healthcare team for guidance tailored to your specific situation.")
+
 
 # Main Streamlit app
 def main():
@@ -77,7 +101,7 @@ def main():
 
         predicted_class = np.argmax(prediction)
         tumor_types = {0: "No Tumor", 1: "Benign Tumor", 2: "Malignant Tumor"}
-        st.subheader(f"Prediction Results: {tumor_types[predicted_class]}")
+        st.subheader(f"Prediction Result: {tumor_types[predicted_class]}")
 
         with open("style.css") as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -91,6 +115,12 @@ def main():
         with col3:
             st.write("Malignant:")
             st.subheader(f"{prediction[0][2] * 100:.0f}%")
+            
+            
+        if st.button("Consultation"):
+            with st.spinner("Generating consultation information..."):
+                display_consultation(prediction)
+
 
 # Run the Streamlit app
 if __name__ == "__main__":
